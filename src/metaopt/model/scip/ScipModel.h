becd7fdab78bef5d798d5cc2d11812ecf19d7041
Arne Mueller <arnem@mi.fu-berlin.de>
2012-10-23 13:58:09 +0200

    added copyright notices and removed unused class ReversedReaction

--- src/metaopt/model/scip/ScipModel.h
+++ src/metaopt/model/scip/ScipModel.h
@@ -1,3 +1,21 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    fast-tfva - efficient thermodynamic constrained flux variability analysis.
+    Copyright (C) 2012  Arne Müller, arne.mueller@fu-berlin.de
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 /*
  * ScipModel.h
  *
